2d2836356f449963a9f0d6b1d277a15aac19d303
Kihoru <thomas.graph.rigal@gmail.com>
2016-03-17 11:01:02 +0100

    basic css

--- resources/views/admin/partials/dashboardNav.blade.php
+++ resources/views/admin/partials/dashboardNav.blade.php
@@ -1,1 +1,11 @@
-<h2>bonjour</h2>
+<ul class="dashboard_nav">
+    <h2>Hello {{$user->name}}</h2>
+
+
+    <li><a href="">Créer un évenement</a></li>
+    <li><a href="">Liste des évenements</a></li>
+    <li><a href="">Utilisateurs</a></li>
+    <li><a href="">Paramètres</a></li>
+    <li><a href="{{url('logout')}}">Déconnexion</a></li><i class="fa fa-sign-out"></i>
+
+</ul>
